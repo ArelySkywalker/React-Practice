4a01dbdae5bdb744483d2e38bf1720ad95f65b06
Arely Miramontes <arelymts@gmail.com>
2019-07-18 19:09:47 -0500

    do the same thing, but as a function instead of class

--- 01_Basics/First_Component/index.js
+++ 01_Basics/First_Component/index.js
@@ -1,13 +1,27 @@
-class Hello extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-			</div>
-		);
-	}
+// CLASS METHOD
+
+// class Hello extends React.Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// FUNCTION METHOD
+
+function Hello() {
+	return (
+		<div>
+			<h1>Hello there!</h1>
+			<h1>Hello there!</h1>
+			<h1>Hello there!</h1>
+		</div>
+	);
 }
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
