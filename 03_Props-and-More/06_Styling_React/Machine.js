526c9cee5e0374ae23fca66bc1747bf3f7989e9f
Arely Miramontes <arelymts@gmail.com>
2019-07-22 19:07:13 -0500

    Styling with React

---
+++ 03_Props-and-More/06_Styling_React/Machine.js
@@ -0,0 +1,19 @@
+class Machine extends React.Component {
+	render () {
+		const { s1, s2, s3 } = this.props;
+		const winner = (s1 === s2) && (s2 === s3);
+		const colors = { fontSize: '50px' };
+		
+		return (
+			<div className="Machine">
+				<p style={ colors }>
+					{ s1 } { s2 } { s3 }
+				</p>
+				<p 
+					className={ winner ? "win" : "lose" }>
+					{ winner ? "Winner!" : "Loser!" }
+				</p>
+			</div>
+		);
+	}
+} 
---
+++ 03_Props-and-More/06_Styling_React/index.js
@@ -0,0 +1,26 @@
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Slot Machine!</h1>
+				<Machine 
+					s1="🍑"
+					s2="🥑"
+					s3="🌶️"
+				/>
+				<Machine 
+					s1="🌶️"
+					s2="🌶️"
+					s3="🌶️"
+				/>
+				<Machine 
+					s1="🍑"
+					s2="🍑"
+					s3="🌶️"
+				/>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root')); 
