d2fcb727515650d14b20f8d837ab78079fb6d9cb
Arely Miramontes <arelymts@gmail.com>
2019-07-30 10:09:08 -0500

    update

--- 04_Create_React_App/fruits_exercise/src/index.js
+++ 04_Create_React_App/fruits_exercise/src/index.js
@@ -1,14 +1,20 @@
 import foods from './foods';
 import { choice, remove } from './helpers';
 
-// Randomy draw a fruit from the arrayl
+// List available foods
+console.log(`Hello, what would you like today? Here is a list of foods we have available: \n${foods}\n\n`);
+
+// Randomy draw a fruit from the array
 let fruit = choice(foods);
 
-console.log(`I’d like one ${fruit}, please.`);
-console.log(`Here you go: ${fruit}`);
-console.log(`Delicious! May I have another?`);
+console.log(`I’d like one ${fruit}, please.\n\n`);
+console.log(`Here you go: ${fruit}\n\n`);
+console.log(`Delicious! May I have another?\n\n`);
 
-// Remove the fruit from the array of fruits
+// Remove the fruit from the array of foods
 let remaining = remove(foods,fruit);
 
-console.log(`I'm sorry, we're all out. We have ${remaining.length} other foods left.`)
+// Print how many food there are left
+console.log(`I'm sorry, we're all out of ${fruit}. We have ${remaining.length} other foods left.\n\n`);
+// Print the remaining foods to choose from
+console.log(`Here is a list of the remaining foods we have: \n${remaining}\n\n`);
